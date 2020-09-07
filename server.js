0d9b211ead4d4e503327de35c48c47466dfe5a8c
rifatNR <rifatnoor92@gmail.com>
2020-09-08 00:10:39 +0600

    Setting Up for production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./Database/MongoConnect')
+const path = require('path')
 
 connectDB()
 
@@ -12,6 +13,15 @@ app.get("/", (req, res)=>res.send("🍆🍆🍆🍆🍆🍆🍆🍆🍆🍆"));
 app.use("/api/reg", require('./Authentication/Register'));
 app.use("/api/login", require('./Authentication/Login'));
 app.use("/api/secrets", require('./route/secrets'));
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('frontend/build'))
+
+    // '*' means get for any route except paths declared above
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'cliend', 'build', 'index.html')))
+}
 
 
 const PORT = process.env.PORT || 5000;
