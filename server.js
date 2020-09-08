d9d80d3472ad1841703cee0351ef56925488b4c3
rifatNR <rifatnoor92@gmail.com>
2020-09-08 22:48:39 +0600

    Fixed a simple problem with frontend/client typo

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'))
 
     // '*' means get for any route except paths declared above
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'cliend', 'build', 'index.html')))
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
 }
 
 
